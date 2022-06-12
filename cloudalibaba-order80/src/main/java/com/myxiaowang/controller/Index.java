5c38b392c8607908d9ef4308c47f79759f5a2e79
Myk <17360402335@163.com>
2022-06-12 16:43:46 +0800

    ribbon的使用

--- cloudalibaba-order80/src/main/java/com/myxiaowang/controller/Index.java
+++ cloudalibaba-order80/src/main/java/com/myxiaowang/controller/Index.java
@@ -1,10 +1,13 @@
 package com.myxiaowang.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import javax.websocket.server.PathParam;
 
 /**
  * @author wck
@@ -20,7 +23,23 @@ public class Index {
     private RestTemplate restTemplate;
 
     @GetMapping("/hello/nacos")
-    public String index(){
+    @SentinelResource(value = "fallback", fallback = "fallback",blockHandler = "blockHandler")
+    //可以忽略某个异常
+    //@SentinelResource(value = "fallback", fallback = "fallback",blockHandler = "blockHandler",exceptionsToIgnore = {RuntimeException.class})
+    public String index(@PathParam("id") Long id){
+        if(id == 4){
+            throw new RuntimeException("id is 4");
+        }
         return restTemplate.getForObject(serverUrl+"/hello/nacos",String.class);
     }
+
+    public String fallback(@PathParam("id") Long id, Throwable e){
+        return "id is 4";
+    }
+
+    public String blockHandler(@PathParam("id") Long id, BlockException e){
+        return "请求次数过多";
+    }
+
+
 }
